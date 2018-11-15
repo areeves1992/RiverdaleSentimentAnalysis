79b0ee5006a92892a01c10045d28a1aa9d5f5032
areeves1992 <areeves1992@gmail.com>
2018-11-14 23:03:49 -0500

    First commit of Riverdale sentiment analysis.
    //TODO: Comments. :) Read me.

---
+++ Get Scripts.R
@@ -0,0 +1,138 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr, plyr, rvest,babynames, tidytext,tidyr,ggplot2, install = TRUE, update = FALSE)
+
+#functions from here http://www.mjdenny.com/Text_Processing_In_R.html
+Clean_String <- function(string){
+  # Lowercase
+  temp <- tolower(string)
+  # Remove everything that is not a number or letter (may want to keep more 
+  # stuff in your actual analyses). 
+  #temp <- stringr::str_replace_all(temp,"[^a-zA-Z\\s]", "")
+  # Shrink down to just one white space
+  temp <- stringr::str_replace_all(temp,"[\\s]+", " ")
+  # Split it
+  temp <- stringr::str_split(temp, " ")[[1]]
+  # Get rid of trailing "" if necessary
+  indexes <- which(temp == "")
+  if(length(indexes) > 0){
+    temp <- temp[-indexes]
+  } 
+  return(temp)
+}
+
+Clean_Text_Block <- function(text){
+  # Get rid of blank lines
+  indexes <- which(text == "")
+  if (length(indexes) > 0) {
+    text <- text[-indexes]
+  }
+  # See if we are left with any valid text:
+  if (length(text) == 0) {
+    cat("There was no text in this document! \n")
+    to_return <- list(num_tokens = 0, 
+                      unique_tokens = 0, 
+                      text = "")
+  } else {
+    # If there is valid text, process it.
+    # Loop through the lines in the text and combine them:
+    clean_text <- NULL
+    for (i in 1:length(text)) {
+      # add them to a vector 
+      clean_text <- c(clean_text, Clean_String(text[i]))
+    }
+    # Calculate the number of tokens and unique tokens and return them in a 
+    # named list object.
+    num_tok <- length(clean_text)
+    num_uniq <- length(unique(clean_text))
+    to_return <- list(num_tokens = num_tok, 
+                      unique_tokens = num_uniq, 
+                      text = clean_text)
+  }
+  
+  return(to_return)
+}
+
+
+
+
+
+
+scriptsURLBase <- "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=riverdale-2017&episode="
+
+
+
+
+rdScriptHome <- "https://www.springfieldspringfield.co.uk/episode_scripts.php?tv-show=riverdale-2017"
+
+
+base <- rdScriptHome %>%
+  read_html() %>%
+  html_nodes(xpath='//*[@id="content_container"]/div[3]/div[2]/div[3]')
+  
+
+baseLinks <- rdScriptHome %>%
+  read_html() %>%
+  html_nodes("a") %>% html_attr("href")
+
+baseLinks <- as.data.frame(baseLinks)
+
+colnames(baseLinks) <- "Links"
+
+baseLinks <- baseLinks %>%
+  filter(grepl("view_episode_scripts", Links))
+
+baseLinks <- baseLinks %>% mutate(SeasonEpisode = gsub("view_episode_scripts.php?tv-show=riverdale-2017&episode=", '', Links, fixed = TRUE))
+
+baseLinks$Season <- paste("Season", substring(baseLinks$SeasonEpisode, 2,3))
+
+text <- rdScriptHome %>%
+  read_html() %>%
+  html_nodes("a") %>% html_text() 
+
+text <- as.data.frame(text)
+
+text <- text %>% filter(grepl("Chapter", text))
+
+text <- as.vector(text$text)
+baseLinks$Episode <- text 
+
+
+
+baseLinks$FullLink <- paste0( scriptsURLBase,baseLinks$Links, sep ='')
+
+scriptTokens <- data.frame(words = character(),wordNumber = numeric(), Episode = character(),EpisodeNum = numeric(), Season = character())
+
+for (i in 1:nrow(baseLinks)){
+  print(i)
+   episode <- baseLinks[i, "Episode"]
+   season <- baseLinks[i, "Season"]
+   episodeClean <- gsub(" ", "", episode, fixed = TRUE)
+   print(episode)
+  script <- baseLinks[i, "FullLink"]%>%
+    read_html() %>%
+    html_nodes(xpath='//*[@id="content_container"]/div[2]/div[2]/div[2]/div[1]') %>% html_text()
+
+  script <- gsub("[\r\n\t]", "", script)
+
+  script <- gsub('\"', '', script, fixed =  TRUE)
+  
+  script <-  as.data.frame(script)
+  
+  token <- script %>%
+    unnest_tokens(word, script, token = stringr::str_split, pattern = " ")
+  
+  token <- token %>%
+    unnest_tokens(word, word)
+  
+  token$wordNumber <-1:nrow(token)
+  
+  token$Episode <- episode
+  
+  token$EpisodeNum <- i
+  
+  token$Season <- season
+  
+  scriptTokens <- rbind(scriptTokens, token)
+  write.table(script, paste0(episodeClean,"_Script.txt",sep = ''))
+
+}
